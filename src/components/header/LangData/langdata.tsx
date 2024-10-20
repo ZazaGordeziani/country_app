e70a1ce9d041fcbdb8cd21d1f0f686d3720d3fae
Zaza Gordeziani <zg.gordeziani@gmail.com>
2024-10-20 21:57:13 +0400

    add language options for header component

---
+++ src/components/header/LangData/langdata.tsx
@@ -0,0 +1,31 @@
+interface LanguageData {
+  [key: string]: {
+    title: string;
+    home: string;
+    booking: string;
+    about: string;
+    contact: string;
+    chooseLanguage: string;
+  };
+}
+
+const langData: LanguageData = {
+  ka: {
+    title: "იმოგზაურე და აღმოაჩინე  ახალი კულტურული სამყარო",
+    home: "მთავარი",
+    booking: "შეკვეთა",
+    about: "ჩვენს შესახებ",
+    contact: "კონტაქტი",
+    chooseLanguage: "ენა",
+  },
+  en: {
+    title: "Travel Around and Explore New Cultures",
+    home: "Home",
+    booking: "Booking",
+    about: "About us",
+    contact: "Contact us",
+    chooseLanguage: "Choose Language",
+  },
+};
+
+export default langData;
---
+++ src/pages/language/views/lang.tsx
@@ -0,0 +1,7 @@
+import LangInfo from "@/pages/language/component/langInfo";
+
+const lang = () => {
+  return <LangInfo />;
+};
+
+export default lang;
